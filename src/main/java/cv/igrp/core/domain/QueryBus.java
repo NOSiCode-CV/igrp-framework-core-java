901b4709b66371e5000d4a1d5c9c3645d4ee3f59
Marcelo.Monteiro <marcelo.monteiro@nosi.cv>
2025-01-30 09:44:56 -0100

    change send to handle in Query bus

--- src/main/java/cv/igrp/core/domain/QueryBus.java
+++ src/main/java/cv/igrp/core/domain/QueryBus.java
@@ -4,33 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * QueryBus interface for sending queries and retrieving responses.
+ * QueryBus interface for handling queries and retrieving responses.
  * Supports dynamic routing of queries to the appropriate handlers.
  */
 public interface QueryBus {
 
     /**
-     * Sends a query to the appropriate handler and returns the response.
+     * Handles a query to the appropriate handler and returns the response.
      *
-     * @param query the query to send
+     * @param query the query to handle
      * @param <C>     the type of the query
      * @param <R>     the type of the response
      * @return the response from the handler
      */
-    <C extends Query, R> R send(C query);
+    <C extends Query, R> R handle(C query);
 
     /**
-     * Sends a list of queries to their appropriate handlers and returns their responses.
+     * Handles a list of queries to their appropriate handlers and returns their responses.
      *
-     * @param queries the list of queries to send
+     * @param queries the list of queries to handle
      * @param <C>      the type of the queries
      * @param <R>      the type of the responses
      * @return a list of responses from the handlers
      */
-    default <C extends Query, R> List<R> sendAll(List<C> queries) {
+    default <C extends Query, R> List<R> handleAll(List<C> queries) {
         List<R> result = new ArrayList<>();
         for (C query : queries) {
-            result.add(this.send(query));
+            result.add(this.handle(query));
         }
         return result;
     }
